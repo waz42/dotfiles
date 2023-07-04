04dbd0b3ff55da72a4966fc7ca9d6b280d25e6bb
waz42 <awazu42@gmail.com>
2023-07-04 09:23:19 +0900

    setup.sh内のcurlコマンドで自己署名証明書を受け入れるように修正

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ if [ -d ~/.zsh ]; then
   echo "exist ./zsh"
 else
   mkdir ~/.zsh
-  curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-  curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-  curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+  curl --insecure -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+  curl --insecure -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  curl --insecure -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 fi
 
 for file in ${DOT_FILES[@]}
